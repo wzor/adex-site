09ee40bc69b713eab1186976f9c063bfec6e698f
Ivo Paunov <ivo.paunov@gmail.com>
2017-08-18 14:59:35 +0300

    fix web3Services path in transferrables

--- pages/api/transferrable/transferrable.js
+++ pages/api/transferrable/transferrable.js
@@ -1,4 +1,4 @@
-const web3Services = require('./../../../services/web3Services')
+const web3Services = require('./../../../services/web3services')
 
 exports.get = function*(request, response) {
 	response.end(JSON.stringify({transferrable: yield web3Services.getTransferable()}))
