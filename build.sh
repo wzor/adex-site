3ae37f7da73f4413baba03610bfe71979c589478
Ivo Georgiev <ivo@linvo.org>
2018-02-22 15:20:47 +0200

    build.sh: add transferrable api

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 mkdir -p dist
+mkdir -p dist/api
 cp -rf public dist/
 cp -rf adex dist
+
 curl http://localhost:4000/ | gunzip > dist/index.html
+curl http://localhost:4000/api/transferrable > dist/api/transferrable
 curl http://localhost:4000/res.css > dist/res.css
 cat scripts/* > dist/scripts.js 
