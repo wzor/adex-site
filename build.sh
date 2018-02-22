761e8a69fd0deb1efeb0a90c0799d5f4bd49dc72
Ivo Georgiev <ivo@linvo.org>
2018-02-22 15:32:11 +0200

    build.sh: add langs

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 mkdir -p dist
-mkdir -p dist/api
+mkdir -p dist/{ru,cn,api}
 cp -rf public dist/
 cp -rf adex dist
 
 curl http://localhost:4000/ | gunzip > dist/index.html
+curl http://localhost:4000/ru | gunzip > dist/ru/index.html
+curl http://localhost:4000/cn | gunzip > dist/cn/index.html
+
 curl http://localhost:4000/api/transferrable > dist/api/transferrable
 curl http://localhost:4000/res.css > dist/res.css
 cat scripts/* > dist/scripts.js 
