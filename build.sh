a4a7c44e8dfe1d14e0b467b4692657232d6806a1
Ivo Georgiev <ivo@linvo.org>
2018-02-22 15:50:34 +0200

    build.sh: sed magic to fix translations

--- build.sh
+++ build.sh
@@ -7,6 +7,20 @@ curl http://localhost:4000/ | gunzip > dist/index.html
 curl http://localhost:4000/ru | gunzip > dist/ru/index.html
 curl http://localhost:4000/cn | gunzip > dist/cn/index.html
 
+sed -i -e 's/\/ru\//\/ru\/index.html/g' dist/index.html
+sed -i -e 's/\/cn\//\/cn\/index.html/g' dist/index.html
+sed -i -e 's/\/en\//\//g' dist/index.html
+
+sed -i -e 's/\/ru\//\/ru\/index.html/g' dist/cn/index.html
+sed -i -e 's/\/cn\//\/cn\/index.html/g' dist/cn/index.html
+sed -i -e 's/\/en\//\//g' dist/cn/index.html
+
+
+sed -i -e 's/\/ru\//\/ru\/index.html/g' dist/ru/index.html
+sed -i -e 's/\/cn\//\/cn\/index.html/g' dist/ru/index.html
+sed -i -e 's/\/en\//\//g' dist/ru/index.html
+
+
 curl http://localhost:4000/api/transferrable > dist/api/transferrable
 curl http://localhost:4000/res.css > dist/res.css
 cat scripts/* > dist/scripts.js 
