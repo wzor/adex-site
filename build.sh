585c4686a0d34fb9e8acf92f5a1c4097ce05df64
Ivo Georgiev <ivo@linvo.org>
2018-02-23 17:35:30 +0200

    build/deploy scripts

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+mkdir -p dist
+mkdir -p dist/{api,about}
+cp -rf public dist/
+cp -rf adex dist
+
+curl http://localhost:4000/ | gunzip > dist/index.html
+sed -i -e 's/\/about\/about\/index.html/g' dist/index.html
+
+curl http://localhost:4000/about | gunzip > dist/about/index.html
+
+curl http://localhost:4000/api/transferrable > dist/api/transferrable
+curl http://localhost:4000/res.css > dist/res.css
+cat scripts/* > dist/scripts.js 
