e87233134a8f083419108bc83858796b525f1332
Ivo Georgiev <ivo@linvo.org>
2018-02-26 14:36:41 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -9,13 +9,14 @@ cp -rf adex dist
 curl http://localhost:4000/ | gunzip > dist/index.html
 
 
+curl http://localhost:4000/about | gunzip > dist/about/index.html
+
 sed -i -e 's/\/about/about\/index.html/g' dist/about/index.html
 sed -i -e 's/\/about/about\/index.html/g' dist/index.html
 
 sed -i -e 's/\/scripts.js/scripts.js/g' dist/index.html
 sed -i -e 's/\/scripts.js/scripts.js/g' dist/about/index.html
 
-curl http://localhost:4000/about | gunzip > dist/about/index.html
 
 curl http://localhost:4000/api/transferrable > dist/api/transferrable
 curl http://localhost:4000/res.css > dist/res.css
